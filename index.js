1a51001d707475700265e5569f50d722b12c51e7
+ <+>
2023-04-02 01:08:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const newId = data.id;
+      document.body.append(newId);
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      document.body.append(errorMessage);
+    });
+  
+    // Return the fetch chain to ensure that the tests run correctly
+    return fetch('http://localhost:3000/users');
+  }
+  
+  
+  
+  
+  
